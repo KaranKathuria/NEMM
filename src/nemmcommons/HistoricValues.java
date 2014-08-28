72ec10d56d4a8e3dccc887e2d08b6f404d59ca39
KaranKathuria <karan.kathuria@optimeering.com>
2014-08-28 10:59:44 +0200

    removed historicvalues object

--- src/nemmcommons/HistoricValues.java
+++
@@ -1,7 +0,0 @@
-/*
- * Version info:
- *     A class containing a arraylist of all historic global values with a tick timestamp.
- *     Last altered data: 20140728
- *     Made by: Karan Kathuria  
- */
-package nemmcommons;
