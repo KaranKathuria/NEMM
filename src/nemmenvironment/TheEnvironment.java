e70354604c1a42e8010393cbd7a1060f4865765c
gbopti <bellgj@hotmail.com>
2014-09-04 16:34:42 +0200

    Calendar static subclass of environment

--- src/nemmenvironment/TheEnvironment.java
+++ src/nemmenvironment/TheEnvironment.java
@@ -1,14 +1,18 @@
 package nemmenvironment;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 
+import repast.simphony.engine.environment.RunEnvironment;
 import repast.simphony.random.RandomHelper;
 import nemmagents.CompanyAgent;
 import nemmagents.CompanyAgent.ActiveAgent;
 import nemmcommons.CommonMethods;
 import nemmcommons.ParameterWrapper;
 import nemmprocesses.ShortTermMarket;
-import nemmtime.NemmCalendar;
+import java.util.Comparator;
 
 public final class TheEnvironment {
 
@@ -121,6 +125,125 @@ public final class TheEnvironment {
 			curRegion.getMyPowerPrice().initMarketSeries(newPrice);
 		}		
 	}
+	// NEMMCALENDAR START ========================================================	
+	public static class NemmCalendar {
+		
+		private int startYear;
+		private int endYear;
+		private int numYears;
+		private int numObligatedPdsInYear;
+		private int numTradePdsInObligatedPd;
+		private int numTradePdsInYear;
+		private int numTicks;
+		private int currentTick;
+		private ArrayList<NemmTime> timeBlocks;
+		
+		
+		/**
+		 * @param startYear
+		 * @param endYear
+		 * @param numObligatedPdsInYear
+		 * @param numTradePdsInObligatedPd
+		 */
+		public NemmCalendar(int startYear, int endYear, int numObligatedPdsInYear,
+				int numTradePdsInObligatedPd) {
+			// should throw errors if start year later than end year, other vals <=0 etc
+			this.currentTick=0;
+			this.startYear = startYear;
+			this.endYear = endYear;
+			this.numObligatedPdsInYear = numObligatedPdsInYear;
+			this.numTradePdsInObligatedPd = numTradePdsInObligatedPd;
+			this.numYears = this.endYear - this.startYear + 1;
+			this.numTradePdsInYear = this.numObligatedPdsInYear*this.numTradePdsInObligatedPd;
+			this.numTicks = this.numYears * this.numTradePdsInYear;
+			timeBlocks = new ArrayList<NemmTime>();
+			
+			int curTick = 0;
+			for (int y = 0; y < numYears; ++y){
+				for (int b = 0; b < this.numObligatedPdsInYear; ++b){
+					for (int t = 0; t < this.numTradePdsInObligatedPd; ++t){
+						NemmTime newBlock = new NemmTime(y,b,t, curTick);
+						timeBlocks.add(newBlock);
+						curTick = curTick+1;
+					}
+				}
+			}
+			Collections.sort(timeBlocks, new NemmTimeCompare());
+		}
+		
+		public int getCurrentTick() {
+			// will grab this from repast
+			return (int) RunEnvironment.getInstance().getCurrentSchedule().getTickCount();
+		}
+
+
+		public NemmTime getTimeBlock(int tickID){
+			return timeBlocks.get(tickID);
+		}
+
+		public int getStartYear() {
+			return startYear;
+		}
+
+		public int getEndYear() {
+			return endYear;
+		}
+
+		public int getNumYears() {
+			return numYears;
+		}
+
+		public int getNumObligatedPdsInYear() {
+			return numObligatedPdsInYear;
+		}
+
+		public int getNumTradePdsInObligatedPd() {
+			return numTradePdsInObligatedPd;
+		}
+
+		public int getNumTradePdsInYear() {
+			return numTradePdsInYear;
+		}
+
+		public int getNumTicks() {
+			return numTicks;
+		}
+		// NEMMTIME START ===================================================
+		public static class NemmTime {
+
+			// This is a structure to concisely hold NEMM time info
+			
+			public int year;
+			public int obligationpdID;
+			public int tradepdID;
+			public Calendar startDate;
+			public Calendar endDate;
+			public int tickIndex;
+			
+			public NemmTime(int yearPd, int oblPd, int trdPd, int tickID) {
+				year = yearPd;
+				obligationpdID = oblPd;
+				tradepdID = trdPd;
+				tickIndex = tickID;
+			}
+				
+			
+		}
+		// NEMMTIME END ===================================================
+		// NEMMTIMECOMPARE START ===================================================
+		class NemmTimeCompare implements Comparator<NemmTime> {
+
+		    @Override
+		    public int compare(NemmTime t1, NemmTime t2) {
+		        // comparison logic based on tick index
+		    	int compareTo = t2.tickIndex > t1.tickIndex ? 1 : (t2.tickIndex < t1.tickIndex ? -1 : 0);
+		        return compareTo;
+		    }
+		}
+		// NEMMTIMECOMPARE END ===================================================
+
+	}
+	// NEMMCALENDAR END ========================================================	
 	
 	public static class GlobalValues {
 		
