602bb9033e546ebecdf251eeeddf5506d2b30733
KaranKathuria <karan.kathuria@optimeering.com>
2014-10-10 16:32:43 +0200

    all fry

--- src/nemmenvironment/FundamentalMarketAnalysis.java
+++ src/nemmenvironment/FundamentalMarketAnalysis.java
@@ -1,5 +1,76 @@
+/*
+ * Version info:
+ *     A class creating the perfect foresight version of alle the LRMC curves for the years ahead. 
+ *     
+ *     Last altered data: 20141010
+ *     Made by: Karan Kathuria
+ */
 package nemmenvironment;
 
-public class FundamentalMarketAnalysis {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
+public class FundamentalMarketAnalysis {
+	
+	//List containing all the fundamental price for the years ahead.
+	private ArrayList<Double> equilibriumpricesyearsahead = new ArrayList<Double>();
+	
+	//In order to preform a fundamental market analysis (with perfect foresight) for all years ahead. A copy of all projects are needed.
+	//public static ArrayList<PowerPlant> projectsunderconstruction_copy = new ArrayList<PowerPlant>();				//PowerPlants currently under construction. Not needed as they are already decided.
+	public static ArrayList<PowerPlant> projectsawaitinginvestmentdecision_copy = new ArrayList<PowerPlant>();		//PowerPlants projects awaiting investment decision
+	public static ArrayList<PowerPlant> projectinprocess_copy = new ArrayList<PowerPlant>();						//All powerplant in process
+	public static ArrayList<PowerPlant> projectsidentifyed_copy = new ArrayList<PowerPlant>();						//All projects identifyed
+	public static ArrayList<PowerPlant> potentialprojects_copy = new ArrayList<PowerPlant>();						//Auto-generated potential projects
+	
+	FundamentalMarketAnalysis() {};													
+	
+	
+	public void runfundamentalmarketanalysis() {
+		//The ArrayLists must be clear as the Collection.copy method would risk keeping some of the original values in the List. 
+		projectsawaitinginvestmentdecision_copy.clear();
+		projectinprocess_copy.clear();
+		projectsidentifyed_copy.clear();
+		potentialprojects_copy.clear();
+		
+		//Then create the copies based on the current year. 
+		Collections.copy(projectsawaitinginvestmentdecision_copy, TheEnvironment.projectsawaitinginvestmentdecision);
+		Collections.copy(projectinprocess_copy, TheEnvironment.projectinprocess);
+		Collections.copy(projectsidentifyed_copy, TheEnvironment.projectsidentifyed);
+		Collections.copy(potentialprojects_copy, TheEnvironment.potentialprojects);
+		
+		//Calcuations begin
+		int currenttick = TheEnvironment.theCalendar.getCurrentTick();
+		int numberofyears = 2035 - /*currentyear */ + 1;
+		int numberofticksinyear = TheEnvironment.theCalendar.getNumTradePdsInYear();
+		
+	for (int i = 0; i < numberofyears; ++i ) {
+		
+		
+		
+		
+		
+		
+		//for each year. Calculate total demand. Take total bank + total production from existing plants and production from plants 
+		double totaldemand = 0;
+		for (Region R : TheEnvironment.allRegions) {															//All regions
+			for (int j = 0; j < numberofticksinyear; j++) { 													//All ticks in a year			
+			totaldemand = totaldemand + R.getMyDemand().getCertDemand(currenttick+(numberofticksinyear*i)+j); } //TheEnvironment.theCalendar.getCurrentTick()+1;
+		}
+			double totalbankandcertproduction = TheEnvironment.GlobalValues.totalmarketphysicalposition; 			//This provides the totalmarket minus or pluss at end of previous tick.
+		for (PowerPlant PP : TheEnvironment.allPowerPlants) { 													//All operational PP
+			for (int j = 0; j < numberofticksinyear; j++) { 													//All ticks in a year
+			totalbankandcertproduction = totalbankandcertproduction + PP.getProduction(currenttick+(numberofticksinyear*i)+j);} //Starting at year i. 
+			}
+		
+		//So we have this comming years demand and production
+		
+		
+				}
+	}
+		
+	}
+		
+	
+	
 }
--- src/nemmenvironment/LRMCCurve.java
+++ src/nemmenvironment/LRMCCurve.java
@@ -8,10 +8,44 @@
 
 package nemmenvironment;
 
+import java.util.ArrayList;
+
 public class LRMCCurve {
 	
-	//Not sure about the fields, but should contain values sufficient to draw the curve. 
-	//Might be that there should be a LRMC-curve for all the years a head. Thats current tick and then x*(12t) ahead.
-	//Guess thsi should be for all the years. 
+	//Not sure about the fields, but should contain values sufficient to draw the curve for a given year. That is the LRMC for a powerplant for a powerplant build that year. This inus the powerprice provides
+	//the fundamental certificate price. 
+	private int yearsahead; 		 //For which year the LRMC-curve is valid. Defined as years ahead from current tick. All years start in januar. 
+	private double equilibriumprice; //Fundamental price for certificates given by intersect of the LRMC for all projects that can provide the certificates demand minus the power price, and the access demand at that year.
+	private ArrayList<curvepair> projectsupplycurve = new ArrayList<curvepair>(); //Sorted by needed cert price, and with accumulated production volume. 
+	private double accessdemand;
+	
+	public class curvepair {
+		private double certpriceneeded;			//The average certprice needed to break even, taking into account LRMC and the number of years with certs and power price.
+		private double annualcertproduction;	//Capacity times LF for the project.
+	
+		public curvepair() {};
+	}
+	
+	public LRMCCurve(int yearsah) {
+		yearsahead = yearsah;
+	}
+	//Methods
 
+	public void calculatelrmccurve() {
+		//Get total bank
+		//Get total demand from when until yearsah
+		//Get total produciton (expected production or perfect foresight). Assuming normal years or perfect foresight or combination.
+		//Fore all projects that can be realised and ar noe already buildt,calcualte LRMC and certprieneeded. sort by cert prce needed. While accessdemand > 0, select and withdraw projects from the list
+		//Add the production form these prosjects to total production (annually). 
+		//Calcualte curve values. 
+		
+	}
+	
+	public void calculateandsetcertpriceneeded(PowerPlant project) {
+		//function setting the certpriceneeded if build in given year for the projects in a list. 
+	}
+	
+	public double getequilibriumprice() {
+		return equilibriumprice;
+	}
 }
