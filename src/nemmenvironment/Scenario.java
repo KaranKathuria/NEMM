6ac5fe7957409eb9453e08d0142cb68bee397757
KaranKathuria <karan.kathuria@optimeering.com>
2015-05-11 13:14:34 +0200

    commitedd added scnario objecct

---
+++ src/nemmenvironment/Scenario.java
@@ -0,0 +1,19 @@
+package nemmenvironment;
+
+import nemmcommons.TickArray;
+import nemmtime.NemmTime;
+
+
+public class Scenario {
+// A storage class for market prices
+	
+	private TickArray seriesValues;
+
+	
+	
+	public Scenario() {
+		seriesValues = new TickArray();
+	}
+
+	
+}
